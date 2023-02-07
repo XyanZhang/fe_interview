caf3e25d2802b0039ada9aadf19a07409f17f7e9
zhangxinyan <zhangxinyan@huajin.com>
2023-02-07 14:41:15 +0800

    feat: promise handle async

---
+++ es6/promise/2.promise/history2/promise.js
@@ -0,0 +1,65 @@
+// 简单版本
+
+
+// promise就是一个类 
+// 1.promise 有三个状态： 成功态（resolve） 失败态（reject） 等待态（pending） (又不成功又不失败)
+// 2.用户自己决定失败的原因和成功的原因  成功和失败也是用户定义的
+// 3.promise 默认执行器时立即执行
+// 4.promise的实例都拥有一个then方法 , 一个参数是成功的回调，另一个失败的回调
+// 5.如果执行函数时发生了异常也会执行失败逻辑
+// 6.如果promise一旦成功就不能失败 ， 反过来也是一样的 (只有等待态的时候才能去更改状态)
+const RESOLVED = 'RESOLVED'; // 成功
+const REJECTED = 'REJECTED'; // 失败
+const PENDING = 'PENDING'; // 等待态
+class Promise {
+    constructor(executor) {
+        this.status = PENDING;
+        this.value = undefined;
+        this.reason = undefined;
+        this.onResolvedCallbacks = []
+        this.onRejectedCallbacks = []
+
+        let resolve = (value) => {
+            if(this.status === PENDING) {
+                this.status = RESOLVED
+                this.value = value
+
+                // 如果resolve在异步函数中，以下等待异步执行完毕再执行resolve，此时再执行then的回调
+                this.onResolvedCallbacks.forEach(fn => fn())
+            }
+        }
+        let reject = (reason) => {
+            if(this.status === PENDING) {
+                this.status = REJECTED
+                this.reason = reason
+                // 同上
+                this.onRejectedCallbacks.forEach(fn => fn())
+            }
+        }
+
+        try {
+            executor(resolve, reject)
+        } catch (error) {
+            reject(error)            
+        }
+    }
+    then(onFulFilled, onRejected){
+        if(this.status === RESOLVED) {
+            onFulFilled(this.value)
+        }
+        if(this.status === REJECTED) {
+            onRejected(this.reason)
+        }
+
+        // 如果是PENDING状态，表示可能是异步， 使用回调函数进行收集, 调用的时机为resolve 或者reject之后
+        if(this.status === PENDING) {
+            this.onResolvedCallbacks.push(() => {
+                onFulfilled(this.value);
+            })
+            this.onRejectedCallbacks.push(() => {
+                onRejected(this.reason);
+            })
+        }
+    }
+}
+module.exports = Promise
