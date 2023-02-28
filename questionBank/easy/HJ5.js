6d9a44da5d55b5f452b5e793510e587480088bec
XyanZhang <1743067175@qq.com>
2023-02-28 22:22:25 +0800

    perf: hj5 another solution

--- questionBank/easy/HJ5.js
+++ questionBank/easy/HJ5.js
@@ -6,5 +6,23 @@ void async function () {
     // Write your code here
     while(line = await readline()){
         console.log(parseInt(line, '16'))
+        console.log(strHandle(line))
     }
 }()
+// 以上是调用parseInt api 取巧办法
+
+// 以下使用运算实现
+function strHandle (line) {
+  // 16进制转10进制
+  let str = line.substr(2);
+  let arr = str.split("");
+  let obj = { A: 10, B: 11, C: 12, D: 13, E: 14, F: 15 };
+  function fn(num) {
+      return num < 10 ? num : obj[num];
+  }
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+      sum = sum + fn(arr[i]) * 16 ** (arr.length - i - 1);
+  }
+  return sum;
+}
