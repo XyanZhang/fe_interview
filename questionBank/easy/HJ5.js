26d284ce43f08592a64098830c2d3879dbe64298
XyanZhang <1743067175@qq.com>
2023-02-28 21:44:07 +0800

    feat(question bank): HJ5

---
+++ questionBank/easy/HJ5.js
@@ -0,0 +1,10 @@
+const rl = require("readline").createInterface({ input: process.stdin });
+var iter = rl[Symbol.asyncIterator]();
+const readline = async () => (await iter.next()).value;
+
+void async function () {
+    // Write your code here
+    while(line = await readline()){
+        console.log(parseInt(line, '16'))
+    }
+}()
