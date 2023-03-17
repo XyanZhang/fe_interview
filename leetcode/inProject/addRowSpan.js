cfab18f9c689203886c68db91035a54c0ca92bdc
XyanZhang <1743067175@qq.com>
2023-03-17 16:54:19 +0800

    refactor: addRowSpan add description

--- leetcode/inProject/addRowSpan.js
+++ leetcode/inProject/addRowSpan.js
@@ -1,14 +1,20 @@
+// 解释：
+// 1. 传入一个数组，数组中的每一项都是一个对象，对象中有一个属性是需要合并的属性
+// 2. 传入一个需要合并的属性
+// 3. 返回一个新的数组，数组中的每一项都是一个对象，对象中有一个属性是需要合并的属性
+// 4. 返回的数组中的每一项都有一个rowSpan属性，表示该项需要合并的行数，如果该项不需要合并，则rowSpan为1
+// 5. 返回的数组中的每一项都有一个colSpan属性，表示该项需要合并的列数，如果该项不需要合并，则colSpan为1
+
 const mergeRows = (rows, key) => {
   rows[0].rowSpan = 1;
-  let idx = 0;
-  return rows.slice(1).reduce(
-    (mergedRows, item, index) => {
+  let idx = 0; // 记录上一个合并的行数的索引
+  return rows.slice(1).reduce((mergedRows, item, index) => {
       if (item[key] === mergedRows[idx][key]) {
-        mergedRows[idx].rowSpan++;
-        item.colSpan = 0;
+        mergedRows[idx].rowSpan++; // 上一个合并的行数加1
+        item.colSpan = 0; // 当前行不显示
       } else {
-        item.rowSpan = 1;
-        idx = index + 1;
+        item.rowSpan = 1; // 当前行不需要合并
+        idx = index + 1; // 记录当前行的索引
       }
       return [...mergedRows, item];
     },
