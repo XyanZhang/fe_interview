fc37f1e006a5306a9272b98128b50ca15a77dd82
XyanZhang <1743067175@qq.com>
2023-08-02 18:10:43 +0800

    feat: react useCallback

--- react-example/src/components/count/index.jsx
+++ react-example/src/components/count/index.jsx
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
 export default function Counter(props) {
   const [count, setCount] = useState(0);
+
+  // 每次点击都会重新渲染，每次都会重新创建handleCount
+  const handleCount = () => {
+    setCount(count + 1)
+  }
+  // 只有count变化时才会重新渲染，每次都会重新创建callbackCount
+  const callbackCount = useCallback(() => {
+    setCount(count - 1)
+  }, [count]);
   return (
     <div>
       <h1>Counter</h1>
       <p>{count}</p>
-      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={handleCount}>+</button>
+      <button onClick={callbackCount}>-</button>
     </div>
   )
 
