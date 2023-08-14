2451222c960f637b0b9ba844b3d0b7ed8c95e1b3
XyanZhang <1743067175@qq.com>
2023-08-14 11:51:58 +0800

    docs: mobx

--- react-example/src/pages/mobxUse/mobx.jsx
+++ react-example/src/pages/mobxUse/mobx.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { observer } from "mobx-react";
+import { observer } from "mobx-react"; // or "mobx-react-lite"
 
 let TodoListOrigin = ({store}) => {
   const onNewTodo = () => {
