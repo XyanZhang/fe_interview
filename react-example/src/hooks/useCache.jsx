9c22ec509239e5c5ce6caf69267745cde299151c
XyanZhang <1743067175@qq.com>
2023-10-21 21:40:31 +0800

    feat: react useCache

---
+++ react-example/src/hooks/useCache.jsx
@@ -0,0 +1,48 @@
+
+let cacheMap;
+
+/**
+* 缓存数据
+* @param cacheKey 缓存的key值
+* @param defaultData 默认值
+* @param expire 缓存时间，默认为-1即无限期，若为正数，单位为ms
+*/
+const useCache = (
+  cacheKey,
+  defaultData,
+  expire = -1
+) => {
+  cacheMap || (cacheMap = new Map());
+
+  // 设置缓存
+  const setCache = (data) => {
+      return expire !== -1
+          ? cacheMap.set(cacheKey, {
+                expire: Date.now() + expire,
+                data,
+            })
+          : cacheMap.set(cacheKey, { data, expire });
+  };
+
+  let result;
+
+  if (cacheMap.has(cacheKey)) {
+      const current = cacheMap.get(cacheKey);
+
+      if (current && current?.expire !== -1 && current?.expire < Date.now()) {
+          // 缓存数据已过期
+          cacheMap.delete(cacheKey);
+          result = defaultData;
+      } else {
+          // 命中缓存
+          result = current?.data;
+      }
+  } else {
+      // 首次使用，返回默认值
+      result = defaultData;
+  }
+
+  return [result, setCache];
+};
+
+export default useCache;
